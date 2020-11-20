351d9d06d90c57e9e15fc5a076dcfe63d0d90a3a
estunosita <59725328+estunosita@users.noreply.github.com>
2020-11-20 11:36:49 +0700

    Add files via upload

---
+++ Pertemuan 1/perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+class perkenalan extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1> Perkenalkan</h1>";
+		echo "<h3>Nama Saya Estu Nosita</h3>";
+		echo "<p>Saya tinggal di Margasari</p>";
+		echo "Hobi saya Menyanyi";
+	}
+}
